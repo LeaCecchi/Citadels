18978f535803c5c0884862b9674a5b943aff826a
Aitor Cachenaut <acachenautiutbayonne.univ-pau.fr>
2020-02-04 18:57:16 +0100

    Intégration du second test

--- src/main/test/com/montaury/citadels/CityTest.java
+++ src/main/test/com/montaury/citadels/CityTest.java
@@ -1,11 +1,36 @@
 package com.montaury.citadels;
 
 
+import com.montaury.citadels.district.Card;
 import org.junit.jupiter.api.Test;
 
 class CityTest {
     @Test
-    public void b(){
-
+    public void testCoutConstructionQuartiers(){
+        Board board = new Board();
+        City city = new City(board);
+        Possession possession=new Possession(0,null);//le score est à 0;
+        city.buildDistrict(Card.HARBOR_3);//+4
+        city.buildDistrict(Card.CATHEDRAL_1);//+5
+        city.buildDistrict(Card.BATTLEFIELD_3);//+3
+        city.buildDistrict(Card.WATCHTOWER_1);//+1
+        int score=city.score(possession);//le score devrait valoir 13
+        assert (score==13);
     }
+
+    @Test
+    public void testCoutConstructionAvecBonusDifferentsTypesQuartiers(){
+        Board board = new Board();
+        City city = new City(board);
+        Possession possession=new Possession(0,null);//le score est à 0;
+        city.buildDistrict(Card.HARBOR_3);//+4
+        city.buildDistrict(Card.CATHEDRAL_1);//+5
+        city.buildDistrict(Card.BATTLEFIELD_3);//+3
+        city.buildDistrict(Card.CASTLE_1);//+4
+        city.buildDistrict(Card.HAUNTED_CITY);//+2
+        int score=city.score(possession);//le score devrait valoir 18 + 3pts bonus
+        assert (score==21);
+    }
+
+    
 }
