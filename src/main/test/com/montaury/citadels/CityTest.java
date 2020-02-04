917ed631c8365a350412fd1e8921580af1c786df
Aitor Cachenaut <acachenautiutbayonne.univ-pau.fr>
2020-02-04 19:02:38 +0100

    Intégration du troisieme test

--- src/main/test/com/montaury/citadels/CityTest.java
+++ src/main/test/com/montaury/citadels/CityTest.java
@@ -32,5 +32,20 @@ class CityTest {
         assert (score==21);
     }
 
-    
+    @Test
+    public void testCoutConstructionAvecBonusCiteComplete(){
+        Board board = new Board();
+        City city = new City(board);
+        Possession possession=new Possession(0,null);//le score est à 0;
+        city.buildDistrict(Card.MANOR_1);//+3
+        city.buildDistrict(Card.CASTLE_1);//+4
+        city.buildDistrict(Card.PALACE_1);//+5
+        city.buildDistrict(Card.WATCHTOWER_1);//+1
+        city.buildDistrict(Card.PRISON_1);//+2
+        city.buildDistrict(Card.BATTLEFIELD_1);//+3
+        city.buildDistrict(Card.FORTRESS_1);//+5
+        int score=city.score(possession);//le score devrait valoir 23 + 4 pts bonus
+        assert (score==27);
+    }
+
 }
